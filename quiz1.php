9b3bf64554a4291aee8c677f682634609680bb74
mallika <jaiswalmallika0126@gmail.com>
2021-02-04 15:41:20 +0530

    first commit

--- quiz1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include 'header.php';?>
-<div class='text-center back text-white' style="height:67vh;padding-top:10%;">
-<div>
-<h1 style="color:#159cad;text-align:center;font-size:70px;color:white;">welcome To online quiz</h></div>
-<div><button class="btn btn-light" style="margin-bottom:10%;"><a href="signup.php">start</a></button><div>
-</div>
-<?php include 'footer.php';?>
